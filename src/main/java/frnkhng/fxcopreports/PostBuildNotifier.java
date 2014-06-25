8ddb59a3bd54563e994a482a1c953d6c86d5b558
Frankie Huang <frnkhng@gmail.com>
2014-06-25 20:57:05 +0800

    draft version

---
+++ src/main/java/frnkhng/fxcopreports/PostBuildNotifier.java
@@ -0,0 +1,14 @@
+package frnkhng.fxcopreports;
+
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+
+@SuppressWarnings("unchecked")
+public class PostBuildNotifier extends Notifier {
+
+	public BuildStepMonitor getRequiredMonitorService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
